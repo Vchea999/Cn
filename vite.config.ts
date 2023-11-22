41a14205dfa3d6ca292aa4c70938032e0e2a93da
Sorn Vichea <113842123+Vchea999@users.noreply.github.com>
2023-11-23 00:54:51 +0700

    Update vite.config.ts
    Add  base: '/Cn/', // Adjust this based on your GitHub repository name
      plugins: [vue()],

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
+// A1
 // vite.config.js
 export default defineConfig({
   base: '/Cn/', // Adjust this based on your GitHub repository name
