a6421cd39263df4ebe6e898ecef4f2db7ef8bbdf
Sorn Vichea <113842123+Vchea999@users.noreply.github.com>
2023-11-23 00:53:28 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
+// vite.config.js
 export default defineConfig({
+  base: '/Cn/', // Adjust this based on your GitHub repository name
   plugins: [vue()],
 });
